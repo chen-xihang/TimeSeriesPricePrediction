d9358249892a21d58022716653f8fc067a6cc551
Xihang Chen <143648286+chen-xihang@users.noreply.github.com>
2026-05-16 20:14:23 +0100

    Update time_series.py

--- ARIMA/time_series.py
+++ ARIMA/time_series.py
@@ -3,16 +3,17 @@ import pandas as pd
 import pandas_datareader.data as web
 import datetime as dt
 import matplotlib.pyplot as plt
-from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.stattools import kpss
+from statsmodels.tsa.stattools import adfuller, kpss
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import numpy as np
 import os
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 os.chdir(os.path.dirname(os.path.realpath("time_series.py")))
 
 start = dt.datetime(1997, 1, 1)
-end = dt.datetime(2007,1,1)
+end = dt.datetime(2006, 12, 31)
 
 # Step 1: Preparing the data, log of monthly average of daily data
 # Federal Reserve Economic Data (FRED) for Brent Spot Price (DCOILBRENTEU)
@@ -22,7 +23,7 @@ brent_m = brent.resample("M").mean()
 # Plotting brent_m
 plt.figure(figsize=(10, 4))
 plt.plot(brent_m, label="Brent spot price (monthly avg)")
-plt.title("Brent crude oil spot price (monthly)")
+plt.title("Brent crude oil spot price (monthly) from 1997 to 2006")
 plt.xlabel("Date")
 plt.ylabel("USD per barrel")
 plt.legend()
@@ -30,24 +31,18 @@ plt.tight_layout()
 plt.savefig("brent_monthly.png", dpi=300, bbox_inches="tight")
 plt.show()
 
-# Log transform
+# Log transform + split into train/validation/test
 brent_log = np.log(brent_m)
-
-plt.figure(figsize=(10, 4))
-plt.plot(brent_log, label="Brent spot log price (monthly avg)")
-plt.title("Brent crude oil spot log price (monthly)")
-plt.xlabel("Date")
-plt.ylabel("USD per barrel")
-plt.legend()
-plt.tight_layout()
-plt.show()
+train_df = brent_log.iloc[brent_log.index <= "2003-01-01", 0]
+validation_df = brent_log.iloc[(brent_log.index > "2003-01-01") & (brent_log.index <= "2005-12-31"), 0]
+test_df = brent_log.iloc[brent_log.index > "2005-12-31", 0]
 
 # Step 2: Decomposing the time series, I part
-brent_log_d1 = brent_log.diff().dropna()
+train_df_d1 = train_df.diff().dropna()
 
 # Visual Test
 plt.figure(figsize=(10, 4))
-plt.plot(brent_log_d1, label="Brent monthly log returns")
+plt.plot(train_df_d1, label="Brent monthly log returns")
 plt.title("Brent monthly log returns first order difference")
 plt.xlabel("Date")
 plt.ylabel("Log return")
@@ -55,25 +50,4 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-series = brent_log_d1.iloc[:, 0]
-
-# Augmented Dickey-Fuller (Root Test)
-adf_result = adfuller(series)
-print(f"Augmented Dickey-Fuller Test Results: This gives p-value: {adf_result[1]:.4g}, for the number of differencing: {adf_result[2]} times.")
-
-# KPSS Test
-kpss_result = kpss(
-    series,
-    regression="c",  
-    nlags="auto"
-)
-print(f"KPSS Test Results: This gives p-value: {kpss_result[1]:.4g}")
-
-# Step 3: Decomposing the time series, AR part
-fig_acf = plot_acf(series, lags=20)
-fig_acf.savefig("acf_brent_log_d1.png", dpi=300, bbox_inches="tight")
-plt.close(fig_acf)
-
-fig_pacf = plot_pacf(series, lags=20)
-fig_pacf.savefig("pacf_brent_log_d1.png", dpi=300, bbox_inches="tight")
-plt.close(fig_pacf)
+series = train_df_d1.iloc[:, 0]
